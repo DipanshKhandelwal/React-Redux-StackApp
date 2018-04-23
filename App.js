d4690cc986d2e9c55ca16a74cab0a32f18ef9f94
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-23 12:07:09 +0530

    Simple list working showing only titles
    Added ListItem component and LibraryList component

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store='createStore'>
-        <View>
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1}} >
           <Header name="StackApp"/>
+          <LibraryList />
         </View>
       </Provider>
     );
