dcb92b1c5d7c614d68480770b75eec5a6b60506c
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-23 10:28:06 +0530

    Add simple empty reducer

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './src/reducers';
 import { Header } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store='createStore'>
         <View>
           <Header name="StackApp"/>
         </View>
+      </Provider>
     );
   }
 }
