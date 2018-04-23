63087caf98c6c038e5e08af0a13215d56047cde6
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-23 20:33:39 +0530

    Add onTouch methods to select and expand required item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import { selectLibrary } from '../actions';
 
 class ListItem extends Component {
+    renderDescription() {
+
+        const { library, expanded } = this.props;
+        const { descriptionStyle} = styles;
+
+        if(expanded) {
+            return (
+                <CardSection>
+                    <Text style={descriptionStyle}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            );
+        }
+    }
+
     render() {
+
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+
         return(
-            <CardSection>
-                <Text  style={ styles.titleStyle }>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text  style={ titleStyle }>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -18,7 +47,18 @@ const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15,
+    },
+    descriptionStyle: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15
     }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
+}; 
+
+export default connect(mapStateToProps, { selectLibrary })(ListItem);
